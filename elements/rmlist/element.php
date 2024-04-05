c600f492901e9e6122b3cbb288527b443fb4ffca
dmitriitsymbal <cymbal@delo-design.ru>
2024-04-05 15:53:17 +0300

    правки

---
+++ elements/rmlist/element.php
@@ -0,0 +1,9 @@
+<?php namespace YOOtheme;
+
+\defined('_JEXEC') or die;
+
+return [
+    'transforms' => [
+        'render' => function ($node) {},
+    ],
+];
--- elements/rmlist/templates/template.php
+++ elements/rmlist/templates/template.php
@@ -9,7 +9,7 @@ $productsListTemplate = $app->input->cookie->get($cookieName, 'grid');
 $cookieName           = 'radicalmart_products-list_ordering';
 $productsListOrdering = $app->input->cookie->get($cookieName);
 
-if ($productsListTemplate !== 'grid')
+if ($productsListTemplate !== 'list')
 {
 	return;
 }
@@ -27,16 +27,7 @@ $grid = $this->el('div', [
 
 	'class' => [
 		'uk-grid',
-		'uk-child-width-[1-{@!grid_default: auto}]{grid_default}',
-		'uk-child-width-[1-{@!grid_small: auto}]{grid_small}@s',
-		'uk-child-width-[1-{@!grid_medium: auto}]{grid_medium}@m',
-		'uk-child-width-[1-{@!grid_large: auto}]{grid_large}@l',
-		'uk-child-width-[1-{@!grid_xlarge: auto}]{grid_xlarge}@xl',
-		'uk-flex-center {@grid_column_align} {@!grid_masonry}',
-		'uk-flex-middle {@grid_row_align} {@!grid_masonry}',
-		$props['grid_column_gap'] == $props['grid_row_gap'] ? 'uk-grid-{grid_column_gap}' : '[uk-grid-column-{grid_column_gap}] [uk-grid-row-{grid_row_gap}]',
-		'uk-grid-divider {@grid_divider} {@!grid_column_gap:collapse} {@!grid_row_gap:collapse}' => count($children) > 1,
-		'uk-grid-match',
+		'uk-child-width-1',
 	],
 
 	'uk-grid' => ' ',
